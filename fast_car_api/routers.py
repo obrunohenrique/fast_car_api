f4f311b7d8bfcb16b34d3c5807510ec7dbd96ef5
Bruno Henrique <bruhenriqueferreirap@gmail.com>
2025-07-07 23:54:20 -0300

    first route - dependency injection, schema

--- fast_car_api/routers.py
+++ fast_car_api/routers.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, status
+from fast_car_api.database import SessionDep
+from fast_car_api.models import Car
 from fast_car_api.schemas import (
     CarPubllic,
     CarSchema
@@ -10,10 +12,14 @@ router = APIRouter(  # informações interessantes de se adicionar
 )
 
 
-@router.post(
+@router.post( 
         '/',
         response_model=CarPubllic,
         status_code=status.HTTP_201_CREATED
 )
-def create_car(car: CarSchema):
-    return car
+def create_car(car: CarSchema, session: SessionDep):
+    car = Car(**car.model_dump())
+    session.add(car)
+    session.commit()
+    session.refresh(car)
+    return car 
--- fast_car_api/schemas.py
+++ fast_car_api/schemas.py
@@ -1,6 +1,7 @@
 from typing import Optional            
 from pydantic import BaseModel
 
+
 class CarSchema(BaseModel):
     brand: str
     model:str
